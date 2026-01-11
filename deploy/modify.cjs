27b0c9ed828e205335eaf3fe0248bbd70f39c5f0
zhoudd <ppnohz@outlook.com>
2026-01-11 15:59:36 +0800

    feat: Add script tag modification to index.html during deployment

---
+++ deploy/modify.cjs
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+
+const indexPath = path.resolve(__dirname, '../dist/index.html');
+const scriptTag = '<script type="module" src="./App.tsx"></script>';
+
+try {
+  let html = fs.readFileSync(indexPath, 'utf-8');
+
+  if (!html.includes(scriptTag)) {
+    // Insert the script tag before the closing </head> tag
+    if (html.includes('</head>')) {
+      html = html.replace('</head>', `  ${scriptTag}\n</head>`);
+      fs.writeFileSync(indexPath, html, 'utf-8');
+      console.log('Successfully added script tag to index.html');
+    } else {
+      console.warn('Could not find </head> tag in index.html');
+    }
+  } else {
+    console.log('Script tag already exists in index.html');
+  }
+} catch (error) {
+  console.error('Error modifying index.html:', error);
+  process.exit(1);
+}
