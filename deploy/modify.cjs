22aea0a393e34ca6f1d7eb3da03ac96e0ea6e34c
zhoudd <ppnohz@outlook.com>
2026-01-11 16:05:19 +0800

    fix: Update script tag source in modify.cjs for correct file reference

--- deploy/modify.cjs
+++ deploy/modify.cjs
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const indexPath = path.resolve(__dirname, '../index.html');
-const scriptTag = '<script type="module" src="./App.tsx"></script>';
+const scriptTag = '<script type="module" src="./Index.tsx"></script>';
 
 try {
   let html = fs.readFileSync(indexPath, 'utf-8');
