52e4391175bfa2dcefdbb4982472d2570cfdbc49
zhoudd <ppnohz@outlook.com>
2026-01-11 16:18:58 +0800

    fix: Correct index.html path case and update script tag insertion point

--- deploy/modify.cjs
+++ deploy/modify.cjs
@@ -1,20 +1,20 @@
 const fs = require('fs');
 const path = require('path');
 
-const indexPath = path.resolve(__dirname, '../index.html');
+const indexPath = path.resolve(__dirname, '../Index.html');
 const scriptTag = '<script  type="module" src="./index.tsx"></script>';
 
 try {
   let html = fs.readFileSync(indexPath, 'utf-8');
 
   if (!html.includes(scriptTag)) {
-    // Insert the script tag before the closing </head> tag
-    if (html.includes('</head>')) {
-      html = html.replace('</head>', `  ${scriptTag}\n</head>`);
+    // Insert the script tag before the closing </body> tag
+    if (html.includes('</body>')) {
+      html = html.replace('</body>', `  ${scriptTag}\n</body>`);
       fs.writeFileSync(indexPath, html, 'utf-8');
       console.log('Successfully added script tag to index.html');
     } else {
-      console.warn('Could not find </head> tag in index.html');
+      console.warn('Could not find </body> tag in index.html');
     }
   } else {
     console.log('Script tag already exists in index.html');
